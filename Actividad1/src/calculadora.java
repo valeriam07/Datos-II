2f27d8d695682e0a7f980aebb5585f2a51ecccfb
valeriam07 <valeriamorales40@gmail.com>
2020-06-11 01:12:53 -0600

    Cambios de la actividad 1

---
+++ Actividad1/src/calculadora.java
@@ -0,0 +1,26 @@
+
+public class calculadora {
+
+    void sumar(int x, int y) {
+        int res = x + y;
+        System.out.println("El resultado de la suma es: " + res);
+    }
+
+    void restar(int x, int y) {
+        int res = x - y;
+        System.out.println("El resultado de la resta es: " + res);
+    }
+
+    void multiplicar(int x, int y) {
+        int res = x * y;
+        System.out.println("El resultado de la multiplicacion es: " + res);
+    }
+
+    void dividir(int x, int y) {
+        int res = x / y;
+        System.out.println("El resultado de la division es: " + res);
+    }
+
+
+}
+
